ab4d1a03d7f1bccdbcb63ba4432e8977835b2d0b
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2015-01-19 13:19:35 -0800

    Suggestion demo

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,4 +5,26 @@ class Quote
   field :source, type: String
   field :url, type: String
   belongs_to :meaning
+  has_many :suggestions, as: :target
+
+  def self.suggestable_fields
+    %(text source url)
+  end
+
+  def self.suggestable_children
+    %()
+  end
+
+  def validate_suggestion(suggestion, errors)
+    quote = self.dup
+    suggestion["data"].each do |field, value|
+      if !Quote.suggestable_fields.include?(field.to_s)
+        errors.add field, "is not suggestable"
+      end
+      quote[field] = value
+    end
+    unless quote.valid?
+      errors.add
+    end
+  end
 end
---
+++ app/models/suggestion.rb
@@ -0,0 +1,24 @@
+class Suggestion
+  include Mongoid::Document
+  belongs_to :parent, class_name: "Suggestion"
+  belongs_to :word
+  belongs_to :user
+  belongs_to :target, polymorphic: true
+
+  field :data, type: Hash
+  field :action, type: String
+
+  def self.actions
+    ["create", "destroy", "change"]
+  end
+
+  state_machine :initial => :new do
+    event :approve do
+      transition :new => :approved
+    end
+
+    state :new do
+      validate {|s| s.target.validate_suggestion(self, errors) }
+    end
+  end
+end
