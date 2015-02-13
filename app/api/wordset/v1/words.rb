74c15a707a22018a34cf06ec48268faf85cf5327
Michael JL Catlin <malrase@gmail.com>
2015-02-13 15:06:53 -0800

    ADding a random route to the api

--- app/api/wordset/v1/words.rb
+++ app/api/wordset/v1/words.rb
@@ -4,9 +4,15 @@ module Wordset
       include Wordset::V1::Defaults
 
       resource :words do
+
+        get '/' do
+          Word.limit(1).offset(rand(Word.count))
+        end
+
         get '/:name' do
           Word.lookup(params[:name])
         end
+
       end
     end
   end
