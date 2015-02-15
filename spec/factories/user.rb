6bcadbe6c7ebc4fc8b313239f7af4af4cc27c2a9
Michael JL Catlin <malrase@gmail.com>
2015-02-14 16:04:21 -0800

    Fixing the tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,5 +4,9 @@ FactoryGirl.define do
     email { Faker::Internet.safe_email }
     password "testtest"
     password_confirmation "testtest"
+    email_opt_in_at Time.now
+    email_opt_in_ip "127.0.0.1"
+    accept_tos_at Time.now
+    accept_tos_ip "127.0.0.1"
   end
 end
