b7a36fbb6f6f3771aceaf20a715deb0633518183
Michael JL Catlin <malrase@gmail.com>
2015-02-17 12:46:38 -0800

    Indexing the activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,8 +3,14 @@ class Activity
   include Mongoid::Timestamps
 
   belongs_to :user
+  index({user_id: 1})
   belongs_to :word
+  index({word_id: 1})
   belongs_to :proposal
+  index({proposal_id: 1})
+  index({type: 1})
+
+  index({created_at: -1})
 
   field :comment, type: String
 end
