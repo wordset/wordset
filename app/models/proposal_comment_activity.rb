d2241765a70559cdc2cc83a4c24d85fe2c0b8724
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2015-02-18 13:52:57 -0800

    migrate old comments

--- app/models/proposal_comment_activity.rb
+++ app/models/proposal_comment_activity.rb
@@ -1,2 +1,16 @@
 class ProposalCommentActivity < Activity
+
+  def self.migrate_comments
+    VoteActivity.ne(comment: nil).each do |va|
+      ProposalCommentActivity.create(
+        comment: va.comment,
+        proposal: va.proposal,
+        word: va.word,
+        user: va.user,
+        created_at: va.created_at,
+        updated_at: va.updated_at
+        )
+      va.update_attributes(comment: nil)
+    end
+  end
 end
