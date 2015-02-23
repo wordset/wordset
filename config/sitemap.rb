bd3f00105b80fbbf952561b1135a40c2ebc06a43
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2015-02-23 14:31:45 -0800

    add mroe urls

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -22,6 +22,7 @@ SitemapGenerator::Sitemap.create do
   #
   Word.each do |word|
     add '/word/#{word.name}', :priority => 0.9, :changefreq => 'weekly'
+    add '/word/#{word.name}/proposals', :priority => 0.2, :changefreq => 'weekly'
   end
 
   add '/users', :priority => 0.5, :changefreq => 'daily'
@@ -29,6 +30,10 @@ SitemapGenerator::Sitemap.create do
 
   User.each do |user|
     add '/user/#{user.username}', :lastmod => user.updated_at
+  end
+
+  Proposal.each do |proposal|
+    add '/proposal/#{proposal.id}', :lastmod => proposal.updated_at
   end
   #
   # Add all articles:
