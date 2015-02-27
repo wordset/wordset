469c438679ac9ae2329dc274a470ee08bb9092a6
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2015-02-27 07:51:59 -0800

    sitemap priority changes

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,31 +2,16 @@
 SitemapGenerator::Sitemap.default_host = "https://www.wordset.org"
 SitemapGenerator::Sitemap.public_path = 'tmp/'
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new
-SitemapGenerator::Sitemap.sitemaps_host = "https://data.wordset.org/"
 
 SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
+
   Word.each do |word|
     add '/word/#{word.name}', :priority => 0.9, :changefreq => 'weekly'
     add '/word/#{word.name}/proposals', :priority => 0.2, :changefreq => 'weekly'
   end
 
   add '/users', :priority => 0.5, :changefreq => 'daily'
-  add '/proposals', :priority => 0.7, :changefreq => 'hourly'
+  add '/proposals', :priority => 0.3, :changefreq => 'hourly'
 
   User.each do |user|
     add '/user/#{user.username}', :lastmod => user.updated_at
@@ -35,10 +20,4 @@ SitemapGenerator::Sitemap.create do
   Proposal.each do |proposal|
     add '/proposal/#{proposal.id}', :lastmod => proposal.updated_at
   end
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
 end
