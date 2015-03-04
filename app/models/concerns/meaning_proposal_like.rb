7bb077d166b9f619b920552d027e2b054d4e717e
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2015-03-04 10:39:39 -0800

    Refactored out the meaning proposal-like functions

---
+++ app/models/concerns/meaning_proposal_like.rb
@@ -0,0 +1,57 @@
+module MeaningProposalLike
+  extend ActiveSupport::Concern
+
+  included do |base|
+    base.before_create :set_before_create
+    base.after_create  :set_proposal
+    base.after_create  :update_project_target
+    base.belongs_to :meaning
+    base.belongs_to :project
+
+    base.validates :meaning,
+                   :presence => true
+
+    base.validate :no_existing_proposal,
+               on: :create
+    base.validate :ensure_project_target_todo,
+               on: :create
+
+    base.index({meaning_id: 1})
+    base.index({_type: 1, meaning_id: 1})
+  end
+
+  def set_before_create
+    self.word = meaning.word
+    self.original = {def: meaning.def,
+                     example: meaning.example,
+                     pos: meaning.entry.pos}
+  end
+
+  def set_proposal
+    meaning.open_proposal = self
+    meaning.save
+  end
+
+  def update_project_target
+    if project
+      project_target.open_proposal!
+    end
+  end
+
+  def project_target
+    ProjectTarget.where(meaning: meaning, project: project).first
+  end
+
+  def no_existing_proposal
+    if meaning.open_proposal
+      self.errors.add :meaning, "already has an open proposal"
+    end
+  end
+
+  def ensure_project_target_todo
+    if project && !project_target.todo?
+      self.errors.add :project_target, "is either pending or fixed"
+    end
+  end
+
+end
--- app/models/propose_meaning_change.rb
+++ app/models/propose_meaning_change.rb
@@ -1,23 +1,6 @@
 class ProposeMeaningChange < Proposal
   include MeaningLike #!!!!! LOOK IN MEANING LIKE!
-
-  belongs_to :meaning
-  belongs_to :proposal
-
-  validates :meaning,
-            :associated => true,
-            :presence => true
-  validate :no_existing_proposal,
-           on: :create
-  validate :ensure_project_target_todo,
-           on: :create
-
-  index({meaning_id: 1})
-  index({_type: 1, meaning_id: 1})
-
-  before_create :set_before_create
-  after_create :set_meaning_proposal
-  after_create :update_project_target
+  include MeaningProposalLike #!!!! ALSO HERE.
 
   def commit!
     meaning.def = self.def
@@ -31,18 +14,6 @@ class ProposeMeaningChange < Proposal
     meaning
   end
 
-  def set_before_create
-    self.word = meaning.word
-    self.original = {def: meaning.def,
-                     example: meaning.example,
-                     pos: meaning.entry.pos}
-  end
-
-  def set_meaning_proposal
-    self.meaning.open_proposal = self
-    self.meaning.save
-  end
-
   def cleanup_proposal!
     meaning.update_attributes(open_proposal: nil)
     if project
@@ -51,26 +22,6 @@ class ProposeMeaningChange < Proposal
     super
   end
 
-  def project_target
-    ProjectTarget.where(meaning: meaning, project: project).first
-  end
 
-  def update_project_target
-    if project
-      project_target.open_proposal!
-    end
-  end
-
-  def no_existing_proposal
-    if meaning.open_proposal
-      self.errors.add :meaning, "already has an open proposal"
-    end
-  end
-
-  def ensure_project_target_todo
-    if project && !project_target.todo?
-      self.errors.add :project_target, "is either pending or fixed"
-    end
-  end
 
 end
