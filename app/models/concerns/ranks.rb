fc91d383f8d1c38bb9a86d816f54768c2afadeec
Michael JL Catlin <malrase@gmail.com>
2015-03-10 12:38:38 -0700

    Senior Editors have over 4000 points

--- app/models/concerns/ranks.rb
+++ app/models/concerns/ranks.rb
@@ -45,6 +45,8 @@ module Ranks
   def rank_id
     if admin?
       return :admin
+    elsif self.points > 4000
+      return :senior_editor
     elsif self.points > 200
       return :editor
     elsif self.points > 100
