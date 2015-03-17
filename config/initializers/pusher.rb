a73e9f6d3a25652303d32d302e3d1c8495c6fc45
Hampton Catlin <hcatlin@gmail.com>
2015-03-17 14:14:38 -0700

    Removing API keys... you tricky people!

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,7 +1,7 @@
 require 'pusher'
 
 if Rails.env == "production"
-  Pusher.url = "http://48d537b460788bef06f4:915af886678748771ce9@api.pusherapp.com/apps/108004"
+  Pusher.url = ENV["PUSHER_URL"]
 else
   Pusher.url = "http://e8039c23fe140e473468:ec8d8cf1d07d87aa8496@api.pusherapp.com/apps/108005"
 end
