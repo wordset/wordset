eebc2a5e3bed9f2dbc2291eb30d88323c4e198f1
Michael JL Catlin <malrase@gmail.com>
2015-03-17 12:36:20 -0700

    Should not notify if user is commenting on own proposal
    The notifier was alerting people when they commented on their own proposal. This should stop that.

--- app/models/proposal_comment_activity.rb
+++ app/models/proposal_comment_activity.rb
@@ -3,5 +3,12 @@ class ProposalCommentActivity < Activity
 
   field :comment, type: String
 
-  after_create :notify_proposal_owner
+  after_create :notify_not_me
+
+  def notify_not_me
+    if self.user != self.proposal.user
+      notify_proposal_owner
+    end
+  end
+
 end
