861a6fef8968d9381afef75eddd4fffe427f2040
Hampton Catlin <hcatlin@gmail.com>
2015-03-23 12:40:26 -0700

    Setup PusherFake correctly in dev.

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,16 +1,12 @@
 
+
 require 'pusher'
-
-if Rails.env != "production"
+if Rails.env == "production"
+  Pusher.url = ENV["PUSHER_URL"]
+  Pusher.logger = Rails.logger
+else
+  Pusher.app_id = "wordset"
+  Pusher.key = "key"
+  Pusher.secret = "mybigsecret"
   require 'pusher-fake/support/base'
-  PusherFake.configure do |configuration|
-    configuration.logger  = Rails.logger
-    configuration.verbose = true
-  end
-  config = PusherFake.configuration
-  ENV["PUSHER_URL"] = "http://PUSHER_API_KEY:PUSHER_API_SECRET@#{config.web_options[:host]}:#{config.web_options[:port]}/PUSHER_APP_ID"
-  puts ENV["PUSHER_URL"]
 end
-
-Pusher.url = ENV["PUSHER_URL"]
-Pusher.logger = Rails.logger
