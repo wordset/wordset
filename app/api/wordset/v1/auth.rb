b257cb485bad7ebb23a82f652ac7afe54512a945
Hampton Catlin <hcatlin@gmail.com>
2015-03-23 12:40:07 -0700

    Namespace the auth methods in the API

--- app/api/wordset/v1/auth.rb
+++ app/api/wordset/v1/auth.rb
@@ -3,28 +3,31 @@ module Wordset
     class Auth < Grape::API
       include Wordset::V1::Defaults
 
-      desc "Get User Auth Token"
-      params do
-        requires :username, type: String
-        requires :password, type: String
-      end
-      post '/login' do
-        username = params[:username]
-        password = params[:password]
+      namespace :auth do
 
-        user = User.where(username: username).first
-        if user && user.valid_password?(password)
-          {username: user.username,
-          auth_key: user.auth_key}
-        else
-          error!({:error_code => 404, :error_message => "Invalid email or password"}, 401)
+        desc "Get User Auth Token"
+        params do
+          requires :username, type: String
+          requires :password, type: String
         end
-      end
+        post '/login' do
+          username = params[:username]
+          password = params[:password]
 
-      desc "Check your session is authorized as a user"
-      get '/authorized' do
-        authorize!
-        true
+          user = User.where(username: username).first
+          if user && user.valid_password?(password)
+            {username: user.username,
+            auth_key: user.auth_key}
+          else
+            error!({:error_code => 404, :error_message => "Invalid email or password"}, 401)
+          end
+        end
+
+        desc "Check your session is authorized as a user"
+        get '/authorized' do
+          authorize!
+          true
+        end
       end
     end
   end
--- spec/api/v1/auth_spec.rb
+++ spec/api/v1/auth_spec.rb
@@ -7,13 +7,13 @@ describe Wordset::V1::Auth do
   it "should allow a user to login" do
     user = create(:user)
 
-    post("/api/v1/login", {username: user.username, password: user.password})
+    post("/api/v1/auth/login", {username: user.username, password: user.password})
     expect(response).to be_successful
     expect_json(username: user.username, auth_key: user.auth_key)
   end
 
   it "should authorize with the proper Authorization bearer info" do
-    get_as(create(:user), "/api/v1/authorized")
+    get_as(create(:user), "/api/v1/auth/authorized")
     expect(response.body).to eq("true")
   end
 end
