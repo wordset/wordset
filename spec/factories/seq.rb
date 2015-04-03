9d980738be3692bdf917dcc0aa82049d61c8c87b
Hampton Catlin <hcatlin@gmail.com>
2015-04-03 09:58:03 -0400

    add in better random test data, to solve some of the issues...

--- spec/factories/seq.rb
+++ spec/factories/seq.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :seq do
-    text { Faker::Lorem.words(2).join(" ") }
+    text { Faker::Lorem.words(4, true).join("") }
     lang { Lang.first || create(:lang) }
   end
 end
