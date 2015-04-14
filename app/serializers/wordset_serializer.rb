e910ec66b6f105ed45fcc2ecaed77677434a6cd9
Michael JL Catlin <malrase@gmail.com>
2015-04-14 09:51:33 -0700

    Serializer for wordset has many meanings now

--- app/serializers/wordset_serializer.rb
+++ app/serializers/wordset_serializer.rb
@@ -1,6 +1,6 @@
 class WordsetSerializer < BaseSerializer
   attributes :id, :name, :lang_id
-  has_many :entries, serializer: EntrySerializer
+  has_many :meanings, serializer: MeaningSerializer
 
   def lang_id
     object.lang.code
