aad21e92657d64a852e23557cb45b405b1572340
Michael JL Catlin <malrase@gmail.com>
2015-04-14 09:52:39 -0700

    Factory for speech_part should create random code/name

--- spec/factories/speech_part.rb
+++ spec/factories/speech_part.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :speech_part do
-    code :adj
-    name "adjective"
+    code { Faker::Lorem.word[0..3] }
+    name { Faker::Lorem.word }
     lang { Lang.first || create(:lang) }
   end
 end
