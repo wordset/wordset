212d949d969fa56870c19fd47389929499666bac
Hampton Catlin <hcatlin@gmail.com>
2015-04-17 07:40:45 -0700

    Disable a lot of the unused parts of rails

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,12 @@ require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
 require "active_model/railtie"
-require "active_job/railtie"
+#require "active_job/railtie"
 #require "active_record/railtie"
-require "action_controller/railtie"
+#require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
-require "sprockets/railtie"
+#require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
