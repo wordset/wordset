ea6a3987b8535d9f4bae6a1871d49fb15a27f50e
Hampton Catlin <hcatlin@gmail.com>
2015-05-11 14:59:12 -0700

    Add an auth method that doesn't
    require a bearer-token

--- app/api/wordsets/v1/auth.rb
+++ app/api/wordsets/v1/auth.rb
@@ -29,6 +29,15 @@ module Wordsets
           true
         end
 
+        params do
+          requires :username
+          requires :auth_key
+        end
+        desc "Used to check a username/key pair, without a real Bearer Token"
+        post '/authorized' do
+          User.where(username: params[:username], auth_key: params[:auth_key]).any?
+        end
+
         desc "pusher configuration options"
         get '/pusher_configuration' do
           {key: PusherFake.configuration.key,
