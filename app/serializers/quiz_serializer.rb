c39685c325378784b3b724efe1fd0fff9ebc6097
Hampton Catlin <hcatlin@gmail.com>
2015-05-15 13:51:37 -0700

    changes for quiz api

--- app/serializers/quiz_serializer.rb
+++ app/serializers/quiz_serializer.rb
@@ -5,16 +5,28 @@ class QuizSerializer < ActiveModel::Serializer
     object.slug
   end
 
+  def image_url
+    object.image.url
+  end
+
   def attributes
     h = super
-    h['questions'] = object.quiz_questions.map do |question|
+    qid = -1
+    h['questions'] = object.quiz_questions.map do |question, index|
+      aid = -1
+      qid += 1
       answers = question.quiz_answers.map do |answer|
+        aid += 1
+        results = answer.result_values.inject({}) { |h, (k, v)| h[k] = v.to_i; h }
         {text: answer.text,
-         result_values: answer.result_values }
+         result_values: results,
+         aid: "#{qid}-#{aid}"}
       end
+
       {text: question.text,
-       image: question.image_url,
-       answers: answers}
+       image_url: question.image.url,
+       answers: answers,
+       qid: qid}
     end
     h['results'] = {}
     object.quiz_results.each do |result|
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,9 +14,11 @@ elsif Rails.env.production?
       aws_secret_access_key:  ENV["AWS_SECRET_ACCESS_KEY"]
     }
     config.fog_directory = "wordset-#{Rails.env}"
+    config.asset_host = "https://data.wordset.org"
   end
 else
   CarrierWave.configure do |config|
     config.storage = :file
+    config.asset_host = "http://localhost:3000"
   end
 end
