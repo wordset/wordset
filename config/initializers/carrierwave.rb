bdd0a6867ffdcf583902224ffcb1bdb677a23a68
Hampton Catlin <hcatlin@gmail.com>
2015-05-16 10:07:14 -0700

    lets try this now

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,12 +7,12 @@ if Rails.env.test? || Rails.env.cucumber?
 elsif Rails.env.production?
   CarrierWave.configure do |config|
     config.storage = :fog
-    # config.fog_provider = 'fog/aws'                        # required
-    # config.fog_credentials = {
-    #   provider:               'AWS',
-    #   aws_access_key_id:      ENV["AWS_ACCESS_KEY_ID"],                        # required
-    #   aws_secret_access_key:  ENV["AWS_SECRET_ACCESS_KEY"]
-    # }
+    config.fog_provider = 'fog/aws'                        # required
+    config.fog_credentials = {
+      provider:               'AWS',
+      aws_access_key_id:      ENV["AWS_ACCESS_KEY_ID"],                        # required
+      aws_secret_access_key:  ENV["AWS_SECRET_ACCESS_KEY"]
+    }
     config.fog_directory = "wordset-#{Rails.env}"
     config.asset_host = "https://data.wordset.org"
   end
