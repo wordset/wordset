626c4ce33a87c59ddf1f21bfa27a79e5362e811b
Hampton Catlin <hcatlin@gmail.com>
2015-05-21 13:25:17 -0700

    Fixing a lot of bugs with trust levels

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    username { Faker::Lorem.characters(10) }
-    email { Faker::Internet.safe_email }
+    sequence(:username) { |n| Faker::Lorem.characters(10) + n.to_s }
+    sequence(:email) { |n| n.to_s + Faker::Internet.safe_email }
     password "testtest"
     password_confirmation "testtest"
     email_opt_in_at Time.now
